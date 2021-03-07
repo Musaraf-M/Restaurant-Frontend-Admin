18afa3c27b5e843bf4b8ea11970ee42a2c052151
Musaraf-M <mohammedmusaraf.mm@psnacet.edu.in>
2021-03-07 12:51:43 +0530

    (add) guard: autherization admin guard added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from '../app/guard/admin-guard.guard';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
+    canActivate: [AdminGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: 'restaurant',
+    loadChildren: () =>
+      import('./restaurant/restaurant.module').then(
+        (m) => m.RestaurantPageModule
+      ),
+    canActivate: [AdminGuard],
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
