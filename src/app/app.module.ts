5c172c61a0b604d569060b381fc3b28bc413c44b
Musaraf-M <mohammedmusaraf.mm@psnacet.edu.in>
2021-03-07 12:45:38 +0530

    (add) app: main page added

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoaderInterceptor } from './interceptor/loader.interceptor';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+
+@NgModule({
+  declarations: [AppComponent, PreloaderComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
